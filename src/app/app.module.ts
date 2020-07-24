a6e00640aa3e1e719946808e52591ff86fec3c53
Rillian Diello Lucas Pires <rillianpires@sescms.com.br>
2020-07-24 13:57:29 -0400

    component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TarefasModule} from './tarefas';
+import { fromEventPattern } from 'rxjs';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    TarefasModule
   ],
   providers: [],
   bootstrap: [AppComponent]
