fecb2c5643eec0984a4481663e38abc245832be1
Rillian Diello <rilliansf@gmail.com>
2020-07-27 23:00:47 -0400

    Delete e update tarefas

--- src/app/tarefas/listar/listar-tarefa.component.ts
+++ src/app/tarefas/listar/listar-tarefa.component.ts
@@ -21,6 +21,22 @@ export class ListarTarefaComponent implements OnInit {
     return this.tarefaService.listarTodos();
   }
 
+  //evento é um evento padrão do navegador q evita atualização da pagina
+  remover($event: any, tarefa: Tarefa) : void{
+    $event.preventDefault(); //por isso usamos a limpeza com o default
+    if(confirm('Deseja Remover a tarefa"' +tarefa.nome+ '"?' )){
+      this.tarefaService.remover(tarefa.id);
+      this.tarefas = this.tarefaService.listarTodos();
+    }
+  }
+
+  alterarStatus(tarefa : Tarefa): void{
+    if(confirm('Deseja alterar o status da tarefa"'+ tarefa.nome+ '"?')){
+      this.tarefaService.alterarStatus(tarefa.id);
+      this.tarefas = this.listarTodos();
+    }
+  }
+
 
 
 }
